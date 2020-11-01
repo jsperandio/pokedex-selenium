d7f2527d169b38a2f503d7f350cf6561f9af023d
João Vitor Esperandio <joaovitor_eb@hotmail.com>
2020-11-01 01:58:05 -0300

    Create new method for lazy extract and other for navigate and extract

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+import csv
 import os
 from typing import List, Callable
+
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec, wait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
 from tqdm import tqdm
-import csv
 
 
 class BaseDriver:
@@ -16,6 +17,9 @@ class BaseDriver:
 
     def close(self):
         self.driver.quit()
+
+    def browser_history_back(self, back_count: int = 1):
+        self.driver.execute_script("window.history.go(-" + back_count.__str__() + ")")
 
     def load_page(self, timeout: int, page_url: str, wait_element: bool, wait_element_id: str):
         self.driver.get(page_url)
@@ -50,7 +54,8 @@ class BaseDriver:
         table_data = reference_table.find_elements_by_css_selector("tbody > tr")
         return table_data
 
-    def iterate_table_data(self,table_data: List[WebElement], handler_item_action: Callable):
+    @staticmethod
+    def iterate_table_data(table_data: List[WebElement], handler_item_action: Callable):
         for row in table_data:
             handler_item_action(row)
 
@@ -59,32 +64,55 @@ class BaseDriver:
         columns = row.find_elements_by_css_selector("td")
         return columns
 
-    def print_table_row_data(self,row):
+    def print_table_row_data(self, row):
         columns: List[WebElement] = self.get_table_row_columns(row)
         print([c.text for c in columns])
+
+    def lazy_table_extract(self,table_headers: List[str], table_data: List[WebElement], csv_name: str) -> list:
+        file = open(csv_name, "w", newline='', encoding='utf-8')
+        writer = csv.writer(file)
+        table_data_extracted: list = [table_headers]
+
+        print("Lazy extracting init")
+
+        for row in tqdm(table_data):
+            columns: List[WebElement] = self.get_table_row_columns(row)
+            table_data_extracted.append([str(c.text).replace('\n', '/').replace('\r', '') for c in columns])
+
+        writer.writerows(table_data_extracted)
+        file.close()
+
+        print(f"Table extracted to: {csv_name}")
+
+        return table_data_extracted
 
     def navigate_and_extract(self, row: WebElement):
         # Get the name column
         name = row.find_element_by_css_selector("td:nth-child(2) > a:nth-child(1)")
+
         # Wait until element has enable to click
         element_invisibility = ec.invisibility_of_element_located((By.ID, "gdpr-confirm"))
         WebDriverWait(self.driver, 3).until(element_invisibility)
         name.click()
+
         # Wait until page load
         element_present = ec.presence_of_element_located((By.CSS_SELECTOR, "ul.list-nav:nth-child(18)"))
         WebDriverWait(self.driver, 3).until(element_present)
+
         # Select Gen 1
         self.driver.find_element_by_css_selector("ul.list-nav:nth-child(18) > li:nth-child(2) > a:nth-child(1)").click()
+
         # Wait until table load
         element_present = ec.presence_of_element_located((
             By.CSS_SELECTOR,
             "#tabs-moves-1 > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > table:nth-child(1)"))
         WebDriverWait(self.driver, 3).until(element_present)
-        table = self.get_table("#tabs-moves-1 > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > table:nth-child(1)")
+        table = self.get_table("#tabs-moves-1 > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > "
+                               "table:nth-child(1)")
         data = self.get_table_data(table)
 
         self.iterate_table_data(data, self.print_table_row_data)
-        self.driver.execute_script("window.history.go(-2)")
+        self.browser_history_back(2)
 
 
 def init_driver(webdriver_path: str = "") -> webdriver:
@@ -172,9 +200,6 @@ def table_data_to_csv(headers: List[str], table_data: List[WebElement], file_nam
     file.close()
 
 
-
-
-
 def main():
     CURR_DIR: str = os.path.abspath(os.getcwd())
     OUTPUT_DIR: str = os.path.join(CURR_DIR, "output")
@@ -189,15 +214,12 @@ def main():
     )
 
     table = base_driver.get_table("#pokedex")
+    headers = base_driver.get_table_headers(table)
     data = base_driver.get_table_data(table)
+    base_driver.lazy_table_extract(table_headers=headers, table_data=data,
+                                   csv_name=os.path.join(OUTPUT_DIR, "pokedex.csv"))
 
-    def print_table_row_data(row):
-        columns: List[WebElement] = base_driver.get_table_row_columns(row)
-        print([c.text for c in columns])
-
-    # base_driver.iterate_table_data(table_data=data, handler_item_action=print_table_row_data)
-
-    base_driver.iterate_table_data(table_data=data, handler_item_action=base_driver.navigate_and_extract)
+    # base_driver.iterate_table_data(table_data=data, handler_item_action=base_driver.print_table_row_data)
     base_driver.close()
 
     # driver: webdriver = init_driver(WEBDRIVER_PATH)
